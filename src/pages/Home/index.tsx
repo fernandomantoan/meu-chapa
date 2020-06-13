09c7650af8c9393a573d9ea5f8a79b497c3a9909
Fernando Mantoan <contato@fernandomantoan.com>
2020-06-13 18:09:01 -0300

    Basic navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,71 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Avatar } from 'react-native-paper';
+import { StyleSheet, View, Text } from "react-native";
+import { Avatar, Button } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons';
 import Photo from "../../../assets/Avatar.png";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Home = () => {
+
+    const navigation = useNavigation();
+
+    function handleNavigateToHealth() {
+        navigation.navigate('Health');
+    }
+
+    function handleNavigateToFreight() {
+        navigation.navigate('Freight');
+    }
+
+    function handleNavigateToHealthVideos() {
+        navigation.navigate('HealthVideos');
+    }
+
+    function handleNavigateToMaps() {
+        navigation.navigate('PointsMap');
+    }
+
+    function handleNavigateToPhones() {
+        navigation.navigate('Freight');
+    }
+
+    function handleNavigateToRewards() {
+        navigation.navigate('Freight');
+    }
+
     return (
-        <View style={styles.container}>
-            {
+        <>
+            <View style={styles.container}>
                 <View style={styles.extendedToolbar}>
                     <Avatar.Image size={120} source={Photo} />
-                </View>}
-        </View>
+                </View>
+                <TouchableOpacity onPress={() => handleNavigateToHealth()}>
+                    <MaterialIcons name="favorite" size={32} />
+                    <Text>Minha Saúde</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleNavigateToFreight()}>
+                    <MaterialIcons name="local-shipping" size={32} />
+                    <Text>Cálculo de Frete</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleNavigateToHealthVideos()}>
+                    <MaterialIcons name="local-movies" size={32} />
+                    <Text>Vídeos</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleNavigateToMaps()}>
+                    <MaterialIcons name="place" size={32} />
+                    <Text>Pontos de Interesse</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleNavigateToPhones()}>
+                    <MaterialIcons name="phone" size={32} />
+                    <Text>Telefones Úteis</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleNavigateToRewards()}>
+                    <MaterialIcons name="shopping-cart" size={32} />
+                    <Text>Recompensas</Text>
+                </TouchableOpacity>
+            </View>
+        </>
     );
 };
 
