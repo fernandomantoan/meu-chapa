fe7b9207d30d93b70b7766afa3faff84ad63bc39
Fernando Mantoan <contato@fernandomantoan.com>
2020-06-13 22:52:04 -0300

    Interest points screen, Interest point detail screen, visual adjustments, divider on recent activities of health screen

---
+++ src/storage.ts
@@ -0,0 +1,21 @@
+import { InterestPoint } from "./entities/Entities";
+
+export const points = [{
+    id: 1,
+    name: 'Posto de Combustível Teste',
+    latitude: -22.998953881362638,
+    longitude: -47.027013803864634,
+    type: 'gas_station'
+}, {
+    id: 2,
+    name: 'Restaurante Teste',
+    latitude: -23.003327354177827,
+    longitude: -47.03332056714976,
+    type: 'restaurant'
+}, {
+    id: 3,
+    name: 'Atendimento',
+    latitude: -22.99305491600461,
+    longitude: -47.03732781352653,
+    type: 'health'
+}] as InterestPoint[];
