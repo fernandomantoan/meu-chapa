72f1a3eb382143553cee8e2badd7667b306e1e33
Alanna Vanzella <alannavanzella@Alannas-MacBook-Pro.local>
2020-06-14 14:06:28 -0300

    Add extra info to freight calculation

--- src/pages/Freight/index.tsx
+++ src/pages/Freight/index.tsx
@@ -1,63 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, SafeAreaView, Slider } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from "@react-navigation/native";
-import { Dropdown } from "react-native-material-dropdown";
+import { Dropdown, DropDownData } from "react-native-material-dropdown";
 import { Appbar, TextInput, Button } from "react-native-paper";
 import styles from './styles';
 
 const Freight = () => {
-    const [axis, setAxis] = useState<number>(0);
-    const [truckageValue, setTruckageValue] = useState<string>("");
+    const [freigthTypes, setFreigthTypes] = useState<DropDownData[]>([]);
+    const [loadTypes, setLoadTypes] = useState<DropDownData[]>([]);
     const [selectedFreightType, setSelectedFreightType] = useState<string>("");
     const [selectedLoadType, setSelectedLoadType] = useState<string>("");
     const [selectedAxisQuantity, setSelectedAxisQuantity] = useState<string>("2");
     const [distance, setDistance] = useState<string>("");
+    const [autonomy, setAutonomy] = useState<string>("");
+    const [averagePrice, setAveragePrice] = useState<string>("");
 
-    const freigthTypes = [
-        {
-            value: "Conjunto Completo ou Caminhão"
-        }, {
-            value: 'Somente Cavalo Mecânico',
-        }, {
-            value: 'Conjunto Completo ou Caminhão de Alto Desempenho (Roda 24h)',
-        }, {
-            value: 'Somente Cavalo Mecânico de Alto Desempenho (Roda 24h)',
-        }];
+    useEffect(() => {
+        setFreigthTypes([
+            {
+                value: "Conjunto Completo ou Caminhão"
+            }, {
+                value: 'Somente Cavalo Mecânico',
+            }, {
+                value: 'Conjunto Completo ou Caminhão de Alto Desempenho (Roda 24h)',
+            }, {
+                value: 'Somente Cavalo Mecânico de Alto Desempenho (Roda 24h)',
+            }
+        ]);
 
-    const loadTypes = [
-        {
-            value: "Granel Líquido",
-            label: "Ble"
-        },
-        {
-            value: "Frigorificada"
-        },
-        {
-            value: "Conteinerizada"
-        },
-        {
-            value: "Carga Geral"
-        },
-        {
-            value: "Neogranel"
-        },
-        {
-            value: "Perigosa (granel sólido)"
-        },
-        {
-            value: "Perigosa (frigorificada)"
-        },
-        {
-            value: "Perigosa (conteinerizada)"
-        },
-        {
-            value: "Perigosa (carga geral)"
-        },
-        {
-            value: "Carga Granel Pressurizada"
-        },
-    ]
+        setLoadTypes([
+            {
+                value: "Granel Líquido",
+            },
+            {
+                value: "Frigorificada"
+            },
+            {
+                value: "Conteinerizada"
+            },
+            {
+                value: "Carga Geral"
+            },
+            {
+                value: "Neogranel"
+            },
+            {
+                value: "Perigosa (granel sólido)"
+            },
+            {
+                value: "Perigosa (frigorificada)"
+            },
+            {
+                value: "Perigosa (conteinerizada)"
+            },
+            {
+                value: "Perigosa (carga geral)"
+            },
+            {
+                value: "Carga Granel Pressurizada"
+            },
+        ]);
+    }, []);
 
     const navigation = useNavigation();
 
@@ -93,10 +97,12 @@ const Freight = () => {
                         <Dropdown
                             label="Tipo do Frete"
                             data={freigthTypes}
+                            value={selectedFreightType}
                             onChangeText={(value) => handleFreightTypeChange(value)} />
                         <Dropdown
                             label="Tipo da Carga"
                             data={loadTypes}
+                            value={selectedLoadType}
                             onChangeText={(value) => handleLoadTypeChange(value)} />
                         <Text style={styles.formLabel}>Quantidade de Eixos ({selectedAxisQuantity} eixos)</Text>
                         <View style={{ flexDirection: "row", alignItems: "center" }}>
@@ -105,11 +111,25 @@ const Freight = () => {
                             <Text style={[styles.formLabel, { color: "green", width: "5%" }]}>9</Text>
                         </View>
                         <TextInput
-                            style={{ marginTop: 16 }}
+                            style={{ marginTop: 16, backgroundColor: 'white' }}
                             label='Distância (km)'
                             value={distance}
                             mode="outlined"
                             onChangeText={text => setDistance(text)}
+                        />
+                        <TextInput
+                            style={{ marginTop: 16, backgroundColor: 'white' }}
+                            label='Autonomia do caminhão (km/litro)'
+                            value={autonomy}
+                            mode="outlined"
+                            onChangeText={text => setAutonomy(text)}
+                        />
+                        <TextInput
+                            style={{ marginTop: 16, backgroundColor: 'white' }}
+                            label='Preço médio do combustível (R$)'
+                            value={averagePrice}
+                            mode="outlined"
+                            onChangeText={text => setAveragePrice(text)}
                         />
                         <Button style={styles.button} compact={true} mode="contained" onPress={() => calculateTruckage()}>
                             Calcular
